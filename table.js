9b95364f5ad16f819586ff1d02c2bc34f1cfc34b
nermin-web <you@example.comnerminbajric88@gmail.com>
2020-10-30 15:19:35 +0100

    input

--- table.js
+++ table.js
@@ -1,25 +1,12 @@
-let arr = [
-    {
-        first_name: 'Test',
-        last_name: 'Last',
-        address: 'Test 1',
-        phone: '123'
-    },
-    {
-        first_name: 'Hamo',
-        last_name: 'Hapic',
-        address: 'Test 12',
-        phone: '1234'
-    },
-    {
-        first_name: 'Baba',
-        last_name: 'Babatunde',
-        address: 'Test 122',
-        phone: '123456'
-    }
-];
-
 function fillTable() {
+    let arr = [
+        {
+            first_name: document.getElementById('first_name').value,
+            last_name: document.getElementById('last_name').value,
+            address: document.getElementById('address').value,
+            phone: document.getElementById('phone').value,
+        }
+    ]
     arr.forEach(function(item) {
         document.getElementById('myTable').innerHTML += '<tr>' 
         + '<td>' + item.first_name +'</td>' 
