8bee05493b01a524ed13cf661c9cc192915f2344
nermin-web <you@example.comnerminbajric88@gmail.com>
2020-10-30 16:47:44 +0100

    input update

--- table.js
+++ table.js
@@ -7,13 +7,26 @@ function fillTable() {
             phone: document.getElementById('phone').value,
         }
     ]
+
+    if (document.getElementById('first_name').value.length == 0) {
+        document.getElementById('warning').innerHTML = '<div class="alert alert-danger mt-2" role="alert"> Field name must have a value </div>';
+        return;
+    } else {
+        document.getElementById('warning').innerText = '';
+        
+    }
+
     arr.forEach(function(item) {
-        document.getElementById('myTable').innerHTML += '<tr>' 
+        document.getElementById('table-body').innerHTML += '<tr>' 
         + '<td>' + item.first_name +'</td>' 
         + '<td>' + item.last_name +'</td>'
         + '<td>' + item.address +'</td>'
         + '<td>' + item.phone +'</td>'
         +'</tr>'
     })
-    
+
+    document.getElementById('first_name').value = '',
+    document.getElementById('last_name').value = '',
+    document.getElementById('address').value = '',
+    document.getElementById('phone').value = ''
 }
