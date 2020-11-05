e80064e370a7d4baffe5bdb5923abfe1495daa52
nermin-web <you@example.comnerminbajric88@gmail.com>
2020-11-05 22:12:13 +0100

    Delete

--- table.js
+++ table.js
@@ -1,32 +1,47 @@
+let test = [];
+let count = 0;
+
+
+
 function fillTable() {
-    let arr = [
-        {
-            first_name: document.getElementById('first_name').value,
-            last_name: document.getElementById('last_name').value,
-            address: document.getElementById('address').value,
-            phone: document.getElementById('phone').value,
-        }
-    ]
+    
+    let arr = {
+        first_name: document.getElementById('first_name').value,
+        last_name: document.getElementById('last_name').value,
+        address: document.getElementById('address').value,
+        phone: document.getElementById('phone').value
+    }
+    test.push(arr);
+
 
     if (document.getElementById('first_name').value.length == 0) {
         document.getElementById('warning').innerHTML = '<div class="alert alert-danger mt-2" role="alert"> Field name must have a value </div>';
         return;
     } else {
         document.getElementById('warning').innerText = '';
+        count++;        
+    }
+    
+
+    document.getElementById('table-body').innerHTML += '<tr id="'+ count + '">' 
+        + '<td>' + document.getElementById('first_name').value +'</td>' 
+        + '<td>' + document.getElementById('last_name').value +'</td>'
+        + '<td>' + document.getElementById('address').value +'</td>'
+        + '<td>' + document.getElementById('phone').value +'</td>'
+        + '<td>' + '<button type="button" class="btn btn-danger mr-2" onclick="deleteRow(' + count + ');">Delete</button>'
+        + '<button type="button"class="btn btn-success">Edit</button></td>'
+        +'</tr>'
         
+
+    let reset = document.getElementsByClassName('reset');
+    for (let i = 0; i < reset.length; i++){
+        reset[i].value = '';
     }
 
-    arr.forEach(function(item) {
-        document.getElementById('table-body').innerHTML += '<tr>' 
-        + '<td>' + item.first_name +'</td>' 
-        + '<td>' + item.last_name +'</td>'
-        + '<td>' + item.address +'</td>'
-        + '<td>' + item.phone +'</td>'
-        +'</tr>'
-    })
+}
 
-    document.getElementById('first_name').value = '',
-    document.getElementById('last_name').value = '',
-    document.getElementById('address').value = '',
-    document.getElementById('phone').value = ''
+
+function deleteRow(id) {
+    this.id = id;
+    document.getElementById(id).style.display = "none";
 }
